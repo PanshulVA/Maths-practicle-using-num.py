7e975467831df820f326360c350affe293ce4c47
Panshul Vikram Aggarwal <panshul.vikram.aggarwal@outlook.com>
2025-09-18 16:29:50 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,6 @@
+import numpy as np
+list_1=([[1], [28], [15],['Panshul'],['says'],['hello']])
+vector_1= np.matrix(list_1)
+print('Given list=', list_1)
+print("The resultant horizontal vecor:")
+print(vector_1)
