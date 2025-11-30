0599963f3637dd2d3ae3f0f0508ae74f3b060cdd
PanshulVA <panshul.vikram.aggarwal@outlook.com>
2025-11-30 14:48:05 +0530

    same

--- prac6.py
+++ prac6.py
@@ -6,8 +6,9 @@ NC= int(input("Enter the number of columns:"))
 print("Enter the entries in a single line (seperated by space)")
 entries = list (map(int, input().split()))
 A=np.array(entries).reshape(NR,NC)
-
 M = sp.Matrix(A)
+
+
 print("Column Space Basis:\n", M.columnspace())
 print("\nRow Space Basis:\n", M.rowspace())
 
