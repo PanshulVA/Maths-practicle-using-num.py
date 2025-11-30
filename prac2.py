cbcf85a762f5bc60e968c41b03474b312a32ba36
PanshulVA <panshul.vikram.aggarwal@outlook.com>
2025-11-30 11:57:07 +0530

    little things could be better

--- prac2.py
+++ prac2.py
@@ -3,7 +3,7 @@ import sympy as sp
 NR= int(input("Enter the number of rows:"))
 NC= int(input("Enter the number of columns:"))
 print("Enter the entries in a single line (seperated by space)")
-entries = list (map(int, input().split()))
+entries = list(map(int, input().split()))
 A=np.array(entries).reshape(NR,NC)
 
 
@@ -13,3 +13,5 @@ rref_matrix, pivots = M.rref()
 print("Row Echelon Form (RREF):\n", rref_matrix)
 print("Rank of Matrix:", M.rank())
 print("Pivot Columns:", pivots)
+
+
